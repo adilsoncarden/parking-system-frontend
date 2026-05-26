86a4d367496199a5f9fc09d18250b070767b4fce
ITP89 <u23231734@utp.edu.pe>
2026-05-26 13:52:20 -0500

    actualizar carritoService.js

--- src/services/carritoService.js
+++ src/services/carritoService.js
@@ -1,10 +1,44 @@
-import { carritosMock } from "../data/condominiosData";
-import { fakeDelay } from "./_apiHelper";
-
-const _carritos = [...carritosMock];
+import api from './api';
 
 export const carritoService = {
-    getAll: () => fakeDelay([..._carritos]),
 
-    getById: (id) => fakeDelay(_carritos.find(c => c.id === id) || null),
+    getAll: async () => {
+        const response = await api.get('/admin/carritos');
+        return response.data;
+    },
+
+    getByCondominio: async (idCondominio) => {
+        const response = await api.get(`/admin/carritos/condominio/${idCondominio}`);
+        return response.data;
+    },
+
+    getByEntrada: async (idEntrada) => {
+        const response = await api.get(`/admin/carritos/entrada/${idEntrada}`);
+        return response.data;
+    },
+
+    getById: async (id) => {
+        const response = await api.get(`/admin/carritos/${id}`);
+        return response.data;
+    },
+
+    create: async (data) => {
+        const response = await api.post('/admin/carritos', data);
+        return response.data;
+    },
+
+    update: async (id, data) => {
+        const response = await api.put(`/admin/carritos/${id}`, data);
+        return response.data;
+    },
+
+    delete: async (id) => {
+        await api.delete(`/admin/carritos/${id}`);
+        return { success: true, id };
+    },
+
+    cambiarDisponibilidad: async (id, disponible) => {
+        const response = await api.patch(`/admin/carritos/${id}/disponibilidad?disponible=${disponible}`);
+        return response.data;
+    },
 };
